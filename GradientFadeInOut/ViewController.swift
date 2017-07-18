a21ff0e49fd015e79f081979375464f106a01f5e
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2017-07-17 23:02:48 -0400

    Update to Swift 3

--- GradientFadeInOut/ViewController.swift
+++ GradientFadeInOut/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     
     var gradient = CAGradientLayer()
 
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         label.text = "Hoping Apple will soon release an updated MacBook Pro. \n#apple #macbook"
     }
@@ -34,34 +34,34 @@ class ViewController: UIViewController {
     
     func setupGradient()
     {
-        let gradientDark = UIColor.blueColor()
-        let gradientLight = UIColor.whiteColor()
+        let gradientDark = UIColor.blue
+        let gradientLight = UIColor.white
         
-        let color1 = gradientDark.CGColor as CGColorRef
-        let color2 = gradientLight.CGColor as CGColorRef
-        let color3 = gradientLight.CGColor as CGColorRef
-        let color4 = gradientDark.CGColor as CGColorRef
+        let color1 = gradientDark.cgColor as CGColor
+        let color2 = gradientLight.cgColor as CGColor
+        let color3 = gradientLight.cgColor as CGColor
+        let color4 = gradientDark.cgColor as CGColor
         
         gradient.colors = [color1, color2, color3, color4]
-        self.view.layer.insertSublayer(gradient, atIndex: 0)
+        self.view.layer.insertSublayer(gradient, at: 0)
     }
     
     func updateGradient()
     {
         gradient.frame = view.bounds
-        gradient.locations = gradientLocations()
+        gradient.locations = gradientLocations() as [NSNumber]
     }
 
     func gradientLocations() -> [CGFloat]
     {
         // Determine screen size and height
-        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        let screenSize: CGRect = UIScreen.main.bounds
         let screenHeight = screenSize.height
 
         // Get the origin of the label
-        let labelOrigin = CGPointMake(label.bounds.origin.x,label.bounds.origin.y);
+        let labelOrigin = CGPoint(x: label.bounds.origin.x,y: label.bounds.origin.y);
         // Convert the label origin to its origin in the superview coordinate system
-        let topInScreen = label.convertPoint(labelOrigin, toView: view)
+        let topInScreen = label.convert(labelOrigin, to: view)
         // Set the gradient stop position above the label based on the y position in superview
         let topGradientStop = topInScreen.y
         // Compute the gradient stop number between 0 and 1 as ratio of screen height
